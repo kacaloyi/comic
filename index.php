75417773b9ec005ae9cc620b7032183417ad5b8b
kacaloyi <realfreeday@163.com>
2021-02-05 08:21:18 +0800

    去掉log,去掉Debug

--- index.php
+++ index.php
@@ -23,14 +23,14 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('PHP 版本必须大于等于5
 define('DIR_SECURE_CONTENT', 'powered by http://www.efucms.com');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);
 
 if(!APP_DEBUG){
 	ini_set('display_errors', false);
 }
 
 if(!APP_DEBUG && !strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'micromessenger') && $_GET['c'] != 'Api'){
-	die('请通过微信访问');
+	//die('请通过微信访问');
 }
 define('APP_PATH','./Application/');
 require './#ThinkPHP/ThinkPHP.php';
