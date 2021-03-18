82de060d356c00ba7a756fa4b660adda6426e40d
kacaloyi <realfreeday@163.com>
2021-03-18 17:27:36 +0800

    去掉不正确的作者显示

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 unset($_GET['m']);
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('PHP 版本必须大于等于5.3.0 !');
 
-define('DIR_SECURE_CONTENT', 'powered by http://www.efucms.com');
+define('DIR_SECURE_CONTENT', 'deney Access!');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', false);
