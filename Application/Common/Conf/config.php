d2662a2437a7f86e356426df12a10b7bb6f0fa44
kacaloyi <realfreeday@163.com>
2021-04-21 22:33:18 +0800

    把轮播图链接改成url

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -67,7 +67,8 @@ return array(
 	//CDN地址 
 	//uploadcdn: cdn映射到本机的本地根目录
 	//urlcdn: cdn外部访问的url根目录
-	'uploadcdn'=>"/Public/file/", 
+	//注意：uploadcdn是本地目录，是个相对目录，前面要带"."。urlcdn是web目录，是绝对的。两个目录都不需要在后面加上反斜杠"/"
+	'uploadcdn'=>"./Public/file", 
 	'urlcdn'=>"https://oss.biquyx.com",
 	
 	//漫画图片地址
